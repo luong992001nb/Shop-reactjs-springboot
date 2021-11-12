48ca358bc70f0e0cabb235b18020e53bcaa496d5
luong992001nb <luongldph11179@fpt.edu.vn>
2021-11-12 20:08:17 +0700

    sua file

--- src/layouAdmin/TabForm.jsx
+++ src/layouAdmin/TabForm.jsx
@@ -2,22 +2,13 @@ import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import Row from 'react-bootstrap/Row'
 import { Col } from 'reactstrap';
-import { creat } from '../api/bookAPI';
-import {useForm} from 'reacr-hook-from'
+
 const TabForm = () => {
 
-    const onHandleAvatar = (e)=>{
-        const file = e.target.files[0];
-        anh || (file.preview = URL.createObjectURL(file))
-        setAnh(file);
-    }
+
      
     const onHandleSubmit = ()=>{
-    setBook({name: ten,theloai,anh.preview,giasach});
-    console.log(book);
-    creat(book).then(()=>{
-     claerFrom();
-    });
+
     }
 
     return (
@@ -26,28 +17,28 @@ const TabForm = () => {
             <Form.Group as={Row}>
                 <Form.Label column sm="2">Tên Sách:</Form.Label>
                 <Col sm="10">
-                    <Form.Control value={ten} onChange={e => setName(e.target.value)} type="text" />
+                    <Form.Control  type="text" />
                 </Col>
             </Form.Group>
             <br />
             <Form.Group as={Row}>
                 <Form.Label column sm="2">Thể Loại:</Form.Label>
                 <Col sm="10">
-                    <Form.Control value={theloai} onChange={e => setTheloai(e.target.value)} type="text" />
+                    <Form.Control  type="text" />
                 </Col>
             </Form.Group>
             <br />
             <Form.Group as={Row}>
                 <Form.Label column sm="2">Giá Sách:</Form.Label>
                 <Col sm="10">
-                    <Form.Control value={giasach} onChange={e => setGia(e.target.value)} type="text" />
+                    <Form.Control  type="text" />
                 </Col>
             </Form.Group>
             <br />
             <Form.Group as={Row}>
-                <Form.Label value={anh} column sm="2">Ảnh Bìa:</Form.Label>
+                <Form.Label  column sm="2">Ảnh Bìa:</Form.Label>
                 <Col sm="10">
-                <Form.Control type="file" onChange={onHandleAvatar} />
+                <Form.Control type="file"  />
                 </Col>
             </Form.Group>
             <br />
